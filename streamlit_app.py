971ced994d6ef264fee7e06fcbe3612dcea17285
Mohamed Gaber <87269358+Mohamedgaber9@users.noreply.github.com>
2024-10-26 02:05:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+# st.text(fruityvice_response.json())
 
 
 
